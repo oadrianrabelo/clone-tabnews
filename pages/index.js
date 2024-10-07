637624b54f49944248857b3ed8b972948a341e93
Ádrian Rabelo <adrianrabeloe10@gmail.com>
2024-10-07 18:27:21 -0400

    retorna projeto para fase de estudo

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,10 @@
 import Head from "next/head";
-import { CartaAberta } from "./carta-aberta";
 function Home() {
   return (
     <>
       <Head>
         <title>Carta aberta</title>
       </Head>
-      <CartaAberta />
     </>
   );
 }
