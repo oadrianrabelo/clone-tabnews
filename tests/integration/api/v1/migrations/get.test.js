dd3a20b1b2f015ba63a20ac9df7f3514f8ea9340
Ádrian Rabelo <adrianrabeloe10@gmail.com>
2025-03-04 15:18:04 +0000

    refactor: moving test files to correct location

---
+++ tests/integration/api/v1/migrations/get.test.js
@@ -0,0 +1,23 @@
+import orchestrator from "tests/orchestrator.js";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+});
+describe("GET /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving pending migrations", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+
+      expect(response.status).toBe(200);
+
+      const responseBody = await response.json();
+      const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+
+      expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+      expect(responseBody.dependencies.database.version).toEqual("16.0");
+      expect(responseBody.dependencies.database.max_connections).toEqual(100);
+      expect(responseBody.dependencies.database.opended_connections).toEqual(1);
+    });
+  });
+});
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,23 +1,20 @@
-import orchestrator from "tests/orchestrator.js";
+import database from "infra/database.js";
+import orchestrator from "tests/orchestrator";
 
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
+  await database.query("drop schema public cascade; create schema public;");
 });
-describe("GET /api/v1/migrations", () => {
+describe("GET /api/v1/status", () => {
   describe("Anonymous user", () => {
-    test("Retrieving pending migrations", async () => {
-      const response = await fetch("http://localhost:3000/api/v1/status");
-
+    test("Retrieving current system status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/migrations");
       expect(response.status).toBe(200);
 
       const responseBody = await response.json();
-      const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
 
-      expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
-
-      expect(responseBody.dependencies.database.version).toEqual("16.0");
-      expect(responseBody.dependencies.database.max_connections).toEqual(100);
-      expect(responseBody.dependencies.database.opended_connections).toEqual(1);
+      expect(Array.isArray(responseBody)).toBe(true);
+      expect(responseBody.length).toBeGreaterThan(0);
     });
   });
 });
