1c53a8b3ad63b017440dc6acbff626362037f2bd
Ádrian Rabelo <adrianrabeloe10@gmail.com>
2025-03-04 14:58:27 +0000

    test: rename all descriptions to use new pattern

---
+++ tests/get.test.js
@@ -0,0 +1,20 @@
+import database from "infra/database.js";
+import orchestrator from "tests/orchestrator";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+  await database.query("drop schema public cascade; create schema public;");
+});
+describe("GET /api/v1/status", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving current system status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/migrations");
+      expect(response.status).toBe(200);
+
+      const responseBody = await response.json();
+
+      expect(Array.isArray(responseBody)).toBe(true);
+      expect(responseBody.length).toBeGreaterThan(0);
+    });
+  });
+});
